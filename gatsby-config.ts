35ecdcded42c3b5b829f77bbe825366183386f53
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2025-01-26 23:59:31 +0700

    first commit

---
+++ gatsby-config.ts
@@ -0,0 +1,56 @@
+import type { GatsbyConfig, PluginRef } from 'gatsby'
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    siteTitle: `2025`,
+    siteTitleAlt: `2025`,
+    siteHeadline: `2025`,
+    siteUrl: `https://gatsby-starter-emilia.vercel.app`,
+    siteDescription: '2025 is here.',
+    siteImage: `/og-image.jpg`,
+    siteLanguage: `en`,
+    author: `@torn4dom4n`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-emilia`,
+      options: {
+        name: `2025`,
+        socialMedia: [{}],
+        showThemeAuthor: false,
+        location: `Vietnam`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `2025`,
+        short_name: `2025`,
+        description: `2025 is here.`,
+        start_url: `/`,
+        background_color: `#fff`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-384x384.png`,
+            sizes: `384x384`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+  ].filter(Boolean) as Array<PluginRef>,
+}
+
+export default config
